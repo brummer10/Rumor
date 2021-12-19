0859410fc27d67742cc9ddd804d43bdeb127e5f9
brummer10 <brummer-@web.de>
2021-12-19 15:18:59 +0100

    New UI

--- Rumor/Rumor.c
+++ Rumor/Rumor.c
@@ -1,7 +1,7 @@
 
-#define CONTROLS 3
+#define CONTROLS 4
 
-#define GUI_ELEMENTS 0
+#define GUI_ELEMENTS 1
 
 #define TAB_ELEMENTS 0
 
@@ -18,7 +18,7 @@ void set_costum_theme(Xputty *main) {
     main->color_scheme->normal = (Colors) {
          /* cairo    / r  / g  / b  / a  /  */
         .fg =       { 0.850, 0.850, 0.850, 1.000},
-        .bg =       { 0.100, 0.100, 0.100, 1.000},
+        .bg =       { 0.121, 0.098, 0.141, 1.000},
         .base =     { 0.000, 0.000, 0.000, 1.000},
         .text =     { 0.900, 0.900, 0.900, 1.000},
         .shadow =   { 0.000, 0.000, 0.000, 0.200},
@@ -77,11 +77,12 @@ void set_costum_theme(Xputty *main) {
 
 void plugin_value_changed(X11_UI *ui, Widget_t *w, PortIndex index) {
     // do special stuff when needed
+    if (index == 2) adj_set_value(ui->widget[3]->adj, adj_get_value(ui->widget[0]->adj));
 }
 
 void plugin_set_window_size(int *w,int *h,const char * plugin_uri, float scale) {
-    (*w) = 451 * scale; //set initial width of main window
-    (*h) = 234 * scale; //set initial height of main window
+    (*w) = 300 * scale; //set initial width of main window
+    (*h) = 344 * scale; //set initial height of main window
 }
 
 const char* plugin_set_name() {
@@ -90,18 +91,22 @@ const char* plugin_set_name() {
 
 void plugin_create_controller_widgets(X11_UI *ui, const char * plugin_uri, float scale) {
     set_costum_theme(&ui->main);
-    widget_get_scaled_png(ui->win, LDVAR(background_150x1_png));
-    ui->widget[0] = add_lv2_image_toggle (ui->widget[0], ui->win, 2, "Bypass", ui, 60,  60, 87 * scale, 116 * scale);
-    widget_get_png(ui->widget[0], LDVAR(switch_82x2_png));
+    ui->elem[0] = add_lv2_image (ui->elem[0], ui->win, -1, "Image5", ui, 30,  30, 240 * scale, 283 * scale);
+    widget_get_scaled_png(ui->elem[0], LDVAR(monster_png));
+    ui->widget[0] = add_lv2_image_toggle (ui->widget[0], ui->elem[0], 2, "", ui, 90,  180, 60 * scale, 60 * scale);
+    widget_get_png(ui->widget[0], LDVAR(footsw_png));
 
-    ui->widget[1] = add_lv2_knob (ui->widget[1], ui->win, 3, "Intensity", ui, 180,  60, 86 * scale, 117 * scale);
-    widget_get_png(ui->widget[1], LDVAR(knob_120x101_png));
+    ui->widget[1] = add_lv2_knob (ui->widget[1], ui->elem[0], 3, "intensity", ui, 0,  30, 91 * scale, 101 * scale);
+    widget_get_png(ui->widget[1], LDVAR(fx_knob_png));
     set_adjustment(ui->widget[1]->adj, 0.000, 0.000, 0.000, 1.000, 0.010, CL_CONTINUOS);
 
-    ui->widget[2] = add_lv2_knob (ui->widget[2], ui->win, 4, "Volume", ui, 300,  60, 86 * scale, 117 * scale);
-    widget_get_png(ui->widget[2], LDVAR(knob_120x101_png));
+    ui->widget[2] = add_lv2_knob (ui->widget[2], ui->elem[0], 4, "volume", ui, 150,  30, 91 * scale, 101 * scale);
+    widget_get_png(ui->widget[2], LDVAR(fx_knob_png));
     set_adjustment(ui->widget[2]->adj, 0.000, 0.000, 0.000, 1.000, 0.010, CL_CONTINUOS);
 
+
+    ui->widget[3] = add_lv2_image_toggle (ui->widget[3], ui->elem[0], 2, "", ui, 98,  135, 44 * scale, 44 * scale);
+    widget_get_png(ui->widget[3], LDVAR(LED_Red_png));
 }
 
 void plugin_cleanup(X11_UI *ui) {
